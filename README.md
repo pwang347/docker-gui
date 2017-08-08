8dfdf3d99ecbcce863fb0b24d90bc192de1e545f
Paul Wang <paul_wang347@hotmail.com>
2017-08-07 18:33:32 -0700

    add dialog for run command (stub until backend implements it)

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Docker GUI
 Web control utility for managing Docker containers and images.
 
-Developed using ReactJS client and Golang backend.
+Developed with ReactJS client and Golang backend.
 
 # Usage
-docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/bin/docker -p:3000:3000 -p:8080:8080 pwang347/docker-gui:latest
+`docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/bin/docker -p:3000:3000 -p:8080:8080 pwang347/docker-gui:latest`
 
 For additional options, see `exec_container.sh`
--- dg-frontend/src/image/Image.js
+++ dg-frontend/src/image/Image.js
@@ -9,6 +9,9 @@ import {
 } from "material-ui/Table";
 import CircularProgress from "material-ui/CircularProgress";
 import RaisedButton from "material-ui/RaisedButton";
+import TextField from "material-ui/TextField";
+import FlatButton from "material-ui/FlatButton";
+import Dialog from "material-ui/Dialog";
 import { truncateLongString, BACKEND_ROOT_URL } from "../common";
 
 export class ImageView extends React.Component {
@@ -17,6 +20,7 @@ export class ImageView extends React.Component {
 
     this.state = {
       ready: false,
+      dialogOpen: false,
       selectedRow: 0,
       count: 0,
       images: []
@@ -50,14 +54,21 @@ export class ImageView extends React.Component {
     this.setState({ selectedRow: key });
   }
 
-  startContainerForSelectedImage() {
+  startContainerForSelectedImage(ports, mounts, action) {
     var key = this.state.selectedRow;
     var selectedImage = this.state.images[key];
     if (selectedImage === undefined) {
       return;
     }
     this.props.toast(
-      "Running image " + truncateLongString(selectedImage.repotags, 5),
+      "Running image " +
+        truncateLongString(selectedImage.repotags, 5) +
+        " [Ports]: " +
+        this.state.runPort +
+        " [Mounts]: " +
+        this.state.runMount +
+        " [Action]: " +
+        this.state.runAction,
       "info"
     );
     var self = this;
@@ -110,14 +121,95 @@ export class ImageView extends React.Component {
         <RaisedButton
           label="Run"
           primary={true}
-          onClick={this.startContainerForSelectedImage.bind(this)}
+          onClick={this.openRunDialog.bind(this)}
         />
       </div>
     );
   }
+  openRunDialog() {
+    this.setState({ dialogOpen: true });
+  }
+  renderRunDialog() {
+    const actions = [
+      <FlatButton
+        type="reset"
+        label="Reset"
+        secondary={true}
+        style={{ float: "left" }}
+      />,
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onClick={this.handleClose.bind(this)}
+      />,
+      <FlatButton type="submit" label="Run" primary={true} />
+    ];
+    const image =
+      this.state.images.length > 0
+        ? this.state.images[this.state.selectedRow].repotags.join(", ")
+        : "null";
+    return (
+      <Dialog title={"Run " + image} modal={true} open={this.state.dialogOpen}>
+        <form
+          action="/"
+          method="POST"
+          onSubmit={e => {
+            e.preventDefault();
+            this.startContainerForSelectedImage();
+            this.handleClose();
+          }}
+        >
+          Set container configurations before running
+          <br />
+          <TextField
+            name="ports"
+            hintText="Ports (space separated)"
+            value={this.state.runPort}
+            onChange={this.handlePortChange.bind(this)}
+          />
+          <br />
+          <TextField
+            name="mounts"
+            hintText="Mounts (space separated)"
+            value={this.state.runMount}
+            onChange={this.handleMountChange.bind(this)}
+          />
+          <br />
+          <TextField
+            name="action"
+            hintText="Run action"
+            value={this.state.runAction}
+            onChange={this.handleActionChange.bind(this)}
+          />
+          <div
+            style={{
+              textAlign: "right",
+              padding: 8,
+              margin: "24px -24px -24px -24px"
+            }}
+          >
+            {actions}
+          </div>
+        </form>
+      </Dialog>
+    );
+  }
+  handleClose() {
+    this.setState({ dialogOpen: false });
+  }
+  handlePortChange(e) {
+    this.setState({ runPort: e.target.value });
+  }
+  handleMountChange(e) {
+    this.setState({ runMount: e.target.value });
+  }
+  handleActionChange(e) {
+    this.setState({ runAction: e.target.value });
+  }
   render() {
     return (
       <div>
+        {this.state.dialogOpen ? this.renderRunDialog() : <div />}
         {this.state.ready ? this.renderTable() : <CircularProgress />}
       </div>
     );
